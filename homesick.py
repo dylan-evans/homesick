18a55b040b0765c07877e80eff9acdc79ada014d
Dylan Evans <dylan@dje.me>
2017-10-02 15:58:39 +1000

    Uses tags to get the file list

--- homesick.py
+++ homesick.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import re
-import argparse
 import logging
+import argparse
+from copy import copy
+from urlparse import urlparse
 from logging import debug, info, error
 
 import yaml
@@ -15,10 +17,11 @@ CONFPATH = os.path.join(DATAPATH, "config.yaml")
 
 def main(cmd_line):
     logging.basicConfig(level=logging.INFO)
+    print cmd_line.config
     with open(cmd_line.config) as src:
         conf = yaml.load(src)
 
-    os.chdir(cmd_line.data)
+    os.chdir(cmd_line.path)
 
     cmd_line.handler(cmd_line, conf)
 
@@ -31,33 +34,43 @@ def sync_handler(cmd_line, conf):
     env = {'$' + key: val for key, val in conf.get('variables', {}).items()}
 
     for host, tags in conf["hosts"].items():
+        url = urlparse(host)
         ctx = {'host:' + host: True}
         if tags:
             ctx.update({'tag:' + tag: True for tag in tags})
         ctx.update(env)
         try:
-            client.connect(host)
-            stdin, stdout, stderr = client.exec_command("echo $HOME")
-            home = stdout.read().strip()
-
-            sftp = client.open_sftp()
-
-            for local, remote in conf["files"].items():
-                info("Uploading '%s' as '%s'", local, remote)
-                with open(local) as src:
-                    with sftp.open(remote.replace("~", home), mode="w") as dst:
-                        stdout = client.exec_command("echo $HOME")[1]
-                        home = stdout.read().strip()
-                        dst.write(pystache.render(src.read(), ctx))
-            client.close()
+            client.connect(
+                hostname=url.hostname,
+                port=url.port or 22,
+                username=url.username,
+                password=url.password
+            )
         except:
-            error("Update failed for '%s'", host)
+            error("Connection failed to '%s'", host)
+            continue
+
+        home = url.path if url.path else "./"
+
+        fileset = copy(conf.get("files", {}).get("all", {}))
+        for tag in tags:
+            fileset.update(conf.get("files", {}).get(tag, {}))
+
+        sftp = client.open_sftp()
+        for local, remote in fileset.items():
+            info("Uploading '%s' as '%s'", local, remote)
+            with open(local) as src:
+                with sftp.open(remote, mode="w") as dst:
+                    stdout = client.exec_command("echo $HOME")[1]
+                    home = stdout.read().strip()
+                    dst.write(pystache.render(src.read(), ctx))
+        client.close()
 
 
 def get_arguments():
     main_parser = argparse.ArgumentParser(description="Sync home directories.")
     main_parser.add_argument("--config", "-c", default=CONFPATH)
-    main_parser.add_argument("--data", "-d", default=DATAPATH)
+    main_parser.add_argument("--path", "-p", default=os.getenv("HOMESICKPATH", DATAPATH))
 
     subparsers = main_parser.add_subparsers()
 
